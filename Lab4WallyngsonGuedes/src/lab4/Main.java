8a070c3f07d4b291d0f81e8864dd64860b7b0967
wallyngson <33876147+wallygue@users.noreply.github.com>
2018-10-13 16:30:24 -0300

    LAB4

--- Lab4WallyngsonGuedes/src/lab4/Main.java
+++ Lab4WallyngsonGuedes/src/lab4/Main.java
@@ -1,5 +1,11 @@
 package lab4;
 
+/**
+ * Este main tem apenas a funcao de dar o start na Interface;
+ * 
+ * @author Wallyngson Guedes, Matricula 117110835;
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
